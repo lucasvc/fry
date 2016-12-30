d7e39d419f2d1cfc01c6748e7857988d387dce9c
Lucas Ventura Carro <useyour.mind@gmail.com>
2016-12-30 17:35:57 +0100

    Actualización repo de Kodi
    - Instalación base incluye el repo mantenido por pipplware.pplware.pt
    - Upgrade controla si ya estaba el repositorio en el fichero de instalación
    - Actualizada versión de Debian a Jessie

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# execute current script as root
+[ `whoami` = root ] || exec sudo $0
 
-sudo apt-get -y install \
+pipplwareRepo="deb http://pipplware.pplware.pt/pipplware/dists/jessie/main/binary /"
+echo "$pipplwareRepo" >> /etc/apt/sources.list.d/pipplware_jessie.list
+
+wget -O - http://pipplware.pplware.pt/pipplware/key.asc | sudo apt-key add -
+
+apt-get -y update
+apt-get -y install \
 	winbind \
 	samba \
 	kodi
+
+apt-get -y upgrade && \
+	apt-get -y dist-upgrade
 
 # http://serverfault.com/questions/352305/why-can-windows-machines-resolve-local-names-when-linux-cant
 # sed "/hosts:*/hosts:* wins/"
--- upgrade-kodi.sh
+++ upgrade-kodi.sh
@@ -2,10 +2,12 @@
 
 [ `whoami` = root ] || exec sudo $0
 
-pipplwareRepo="deb http://pipplware.pplware.pt/pipplware/dists/stable/main/binary /"
-echo "$pipplwareRepo" >> /etc/apt/sources.list
+pipplwareRepo="deb http://pipplware.pplware.pt/pipplware/dists/jessie/main/binary /"
+sourcesFile=/etc/apt/sources.list.d/pipplware_jessie.list
+if ! grep -q "pipplware.pplware.pt" "$sourcesFile"; then
+	echo "$pipplwareRepo" >> "$sourcesFile"
+	wget -O - http://pipplware.pplware.pt/pipplware/key.asc | sudo apt-key add -
+fi
 
-apt-get -y update
+apt-get -y update 
 apt-get -y dist-upgrade
-
-# sed -i '' '/$pipplwareRepo/d' ./infile
